f54a9ee67ea66e0a4953b8a4662d26a4773e1002
= <=>
2020-10-01 21:06:17 -0300

    Hardcoded text replaced with constants from assets/texts.py file in DisplotScreen

--- assets/texts.py
+++ assets/texts.py
@@ -23,18 +23,30 @@ DIALOG_KEY_ERROR_LINEPLOT = "Los ejes X e Y que ingresaste no son correctos"
 # BACK TO HOME BUTTON TEXT
 BACK_TO_HOME_BUTTON_TEXT = "Volver al inicio"
 
+# FILENAME TEXT INPUT
+FILENAME_TEXTINPUT_TEXT = "Nombre del archivo"
+FILENAME_TEXTINPUT_HELPER_TEXT = "Ej: prueba.csv"
+
 # HomeScreen TEXTS
 DISPLOT_SCREEN_BUTTON_TEXT = 'Gráfico de barras'
 LINEPLOT_SCREEN_BUTTON_TEXT = 'Gráfico de líneas'
 
+PLOT_TITLE_TEXT = "Título del gráfico"
+PLOT_TITLE_HELPER_TEXT = "Ej: Evolución de los casos de COVID-19"
+SHOW_PLOT_BUTTON_TEXT = "Generar gráfico"
+
 # LineplotScreen TEXTS
-LINEPLOT_TEXT_1 = 'Realiza un gráfico de líneas'
-FILENAME_TEXTINPUT_TEXT = "Nombre del archivo"
-FILENAME_TEXTINPUT_HELPER_TEXT = "Ej: prueba.csv"
+LINEPLOT_TEXT_1 = "Realiza un gráfico de líneas"
 X_AXIS_TEXT = "Nombre del Eje X"
 Y_AXIS_TEXT = "Nombre del Eje Y"
 X_LABEL_TEXT = "Etiqueta del Eje X"
 Y_LABEL_TEXT = "Etiqueta del Eje Y"
-PLOT_TITLE_TEXT = "Título del gráfico"
-PLOT_TITLE_HELPER_TEXT = "Ej: Evolución de los casos de COVID-19"
-SHOW_PLOT_BUTTON_TEXT = "Generar gráfico"
+
+# DisplotScreen TEXTS
+DISPLOT_TEXT_1 = "Realiza un gráfico de barras"
+COLUMN_TO_PLOT_TEXT = "Nombre de la columna a graficar:"
+COLUMN_TO_PLOT_HELPER_TEXT = "Ej: edad"
+X_LABEL_TEXT = "Etiqueta del Eje X"
+X_LABEL_HELPER_TEXT = "Ej: Edades"
+Y_LABEL_TEXT = "Etiqueta del Eje Y:"
+Y_LABEL_HELPER_TEXT = "Ej: Número de casos"
